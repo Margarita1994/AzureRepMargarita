611f9e3cb80bd91ea3e11cfa79b53c993feb7fbf
Margarita <mbitadze@yandex.ru>
2015-04-08 10:49:56 +0400

    first commit

---
+++ app/server.js
@@ -0,0 +1,11 @@
+var http = require("http"),
+    server;
+
+server = http.createServer(function (req, res) {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end("Hello World!\n");
+});
+
+server.listen(1337);
+
+console.log("Server running on port 3000");
