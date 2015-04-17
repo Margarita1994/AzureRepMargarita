b7902ee2a22596f569ec1039e6d748598dd2a8f0
Margarita <mbitadze@yandex.ru>
2015-04-17 14:07:49 +0400

    my getpost1

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(express.static(__dirname + "/client"));
 // создадим HTTP-сервер на базе Express
 http.createServer(app).listen(port);
 
-// app.get("/someway.json", function (req, res) {
-// res.json(coolObject);
-// });
+ app.get("/someway.json", function (req, res) {
+res.json(coolObject);
+});
 
 
 
