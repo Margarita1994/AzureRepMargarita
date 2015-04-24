17ecfe688fe074201401056e9541a77a8ba49fa7
Margarita <mbitadze@yandex.ru>
2015-04-24 14:21:45 +0400

    sj bj j. j .11

--- stream.js
+++ stream.js
@@ -1,35 +1,19 @@
-var ntwitter = require("ntwitter"),
-
+var stream = function () {
+	var ntwitter = require("ntwitter"),
 credentials = require("./credentials.json"),
-
 twitter;
 
-// настроим наш объект twitter
-
 twitter = ntwitter(credentials);
-
-// настроим поток twitter с тремя параметрами, разделенными запятыми
-
 twitter.stream(
-
-// первый параметр — строка
-
 "statuses/filter",
-
-// второй параметр — объект, содержащий массив со словами, к. мы ищем
-
-{ "track": ["awesome", "cool", "rad", "gnarly", "groovy" }
-
-// третий параметр — обратный вызов, срабатывающий, когда поток создан
+{ "track": ["awesome"]}
 
 function(stream) {
-
 stream.on("data", function(tweet) {
-
-console.log(tweet.text);
-
+//console.log(tweet.text);
 });
 
 }
-
-);
+return tweet.text;
+};
+module.exports = stream;
